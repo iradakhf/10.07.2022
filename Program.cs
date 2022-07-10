14dbd37f4544e344a9e2e5f93ec0ab7ea0a10ef7
iradakhf <iradakhf@code.edu.az>
2022-07-10 14:39:08 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region
+            //Create a C# program that requests three names of people from the user and stores them in an array 
+            //of objects of type Person. To do this, first create a Person class that has a Name property of
+            //type string and override the ToString() method.
+            #endregion
+            Person person = new Person();
+            person.Greet();
+
+            Student student = new Student();
+            student.SetAge(45);
+          
+            student.ShowAge();
+            student.Greet();
+
+            Teacher teacher = new Teacher();
+            teacher.Greet();
+            teacher.SetAge(12);
+            teacher.Explain();
+            
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("i am studying");
+        }
+        public void ShowAge()
+        {
+            Console.WriteLine(age);
+            
+        }
+    }
+}
